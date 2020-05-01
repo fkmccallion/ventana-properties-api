2d003a775b22958c1551112191b71e5189268a36
github username <github email address>
2020-05-01 06:32:26 -0700

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   resources :property_images
   resources :properties
   resources :agents
+
+  root 'open_houses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

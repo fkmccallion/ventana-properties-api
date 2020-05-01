831f19bb4e02035123f7dd61de929256dab6f003
github username <github email address>
2020-04-30 20:20:00 -0700

    update rails to 2.6.6

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_04_24_161102) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -51,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_161102) do
     t.string "img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "property_id"
+    t.bigint "property_id"
     t.index ["property_id"], name: "index_images_on_property_id"
   end
 
@@ -61,7 +64,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_161102) do
     t.datetime "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "property_id"
+    t.bigint "property_id"
     t.index ["property_id"], name: "index_open_houses_on_property_id"
   end
 
@@ -74,7 +77,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_161102) do
     t.boolean "sold"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "agent_id"
+    t.bigint "agent_id"
     t.float "bed"
     t.float "bath"
     t.integer "sqft"
@@ -93,4 +96,8 @@ ActiveRecord::Schema.define(version: 2020_04_24_161102) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "images", "properties"
+  add_foreign_key "open_houses", "properties"
+  add_foreign_key "properties", "agents"
 end
